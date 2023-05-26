248559f1b3b3317a6048b121f9ed704cc3412b84
Ahmed Mlik <ahmedmlik@live.fr>
2023-05-26 21:00:08 +0100

    26/05/2023

--- cogitel.cs
+++ cogitel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
